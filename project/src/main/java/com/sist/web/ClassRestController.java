0f29fbbea57b12d53d3417d4d72052b9660b0a11
kyunglyn <amy3249@naver.com>
2023-03-13 18:48:35 +0900

    feat:상세보기 관련 기능 추가

--- project/src/main/java/com/sist/web/ClassRestController.java
+++ project/src/main/java/com/sist/web/ClassRestController.java
@@ -1,23 +1,25 @@
 package com.sist.web;
 
-import java.util.List;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import com.sist.dao.*;
 import com.sist.vo.*;
 @RestController
 public class ClassRestController {
 	@Autowired
-	private ClassDAO dao;
+	private ClassService service;
 	
 	@GetMapping(value="class/class_cate_vue.do",produces="text/plain;charset=utf-8")
 	public String class_cate_vue() {
-		List<CategoryVO> list=dao.classCateData();
+		List<CategoryVO> list=service.classCateData();
 	     JSONArray arr=new JSONArray();
 	     for(CategoryVO vo:list)
 	     {
@@ -31,7 +33,7 @@ public class ClassRestController {
 	
 	@GetMapping(value="class/class_detail_cate_vue.do",produces="text/plain;charset=utf-8")
 	public String class_detail_cate_vue(int cateno) {
-		List<CategoryDetailVO> list=dao.classCateDetailData(cateno);
+		List<CategoryDetailVO> list=service.classCateDetailData(cateno);
 		JSONArray arr=new JSONArray();
 		for(CategoryDetailVO vo:list)
 		{
@@ -49,7 +51,7 @@ public class ClassRestController {
 		Map map=new HashMap();
 		map.put("cateno", cateno);
 		map.put("detail_cateno", detail_cateno);
-		List<ClassDetailVO> list=dao.classListData(map);
+		List<ClassDetailVO> list=service.classListData(map);
 		JSONArray arr=new JSONArray();
 		for(ClassDetailVO vo:list)
 		{
@@ -84,5 +86,84 @@ public class ClassRestController {
 		return arr.toJSONString();
 	}
 	
+	@GetMapping(value="class/cookie_data_vue.do",produces = "text/plain;charset=UTF-8")
+	public String class_cookie_data(HttpServletRequest request)
+	{
+		Cookie[] cookies=request.getCookies();
+		List<ClassDetailVO> list=new ArrayList<ClassDetailVO>();
+		if(cookies!=null)
+		{
+			for(int i=cookies.length-1;i>=0;i--)
+			{
+				if(cookies[i].getName().startsWith("classHaru"))
+				{
+					String cno=cookies[i].getValue();
+					ClassDetailVO vo=service.classDetailData(Integer.parseInt(cno));
+					list.add(vo);
+				}
+			}
+		}
+		JSONArray arr=new JSONArray();
+		int i=0;
+		for(ClassDetailVO vo:list)
+		{
+			if(i>5) break;
+			JSONObject obj=new JSONObject();
+			obj.put("cno", vo.getCno());
+			obj.put("cateno", vo.getCateno());
+			obj.put("detail_cateno", vo.getDetail_cateno());
+			obj.put("title", vo.getTitle());
+			obj.put("image", vo.getImage());
+
+			arr.add(obj);
+			i++;
+		}
+		return arr.toJSONString();
+	}
 	
+	@GetMapping(value="class/class_detail_vue.do",produces = "text/plain;charset=UTF-8")
+	public String class_detail_vue(int cno)
+	{
+		ClassDetailVO vo=service.classDetailData(cno);
+		
+		JSONObject obj=new JSONObject();
+		obj.put("cno", vo.getCno());
+		obj.put("cateno", vo.getCateno());
+		obj.put("detail_cateno", vo.getDetail_cateno());
+		obj.put("title", vo.getTitle());
+		String image=vo.getImage();
+		String image1=image.substring(0,image.indexOf("^"));
+		String image2=image.substring(image.indexOf("^")+1);
+//        image=image.substring(0,image.indexOf("^"));
+        obj.put("image1", image1);
+        obj.put("image2", image2);
+		obj.put("tno", vo.getTno());
+		String place=vo.getPlace();
+		place=place.substring(0,place.indexOf("^"));
+		obj.put("place", place);
+		String location=vo.getLocation();
+		location=location.substring(0,location.indexOf("^"));
+		obj.put("location", location);
+		String schedule=vo.getSchedule();
+        schedule=schedule.substring(0,schedule.indexOf("^"));
+        obj.put("schedule", schedule);
+		obj.put("notice", vo.getNotice());
+		obj.put("time", vo.getTime());
+		obj.put("perprice", vo.getPerprice());
+		obj.put("totalprice", vo.getTotalprice());
+		obj.put("summary", vo.getSummary());
+		obj.put("target", vo.getTarget());
+		obj.put("tutor_intro", vo.getTutor_intro());
+		obj.put("class_intro", vo.getClass_intro());
+		obj.put("class_curri", vo.getClass_curri());
+		obj.put("class_video", vo.getClass_video());
+		obj.put("onoff", vo.getOnoff());
+		obj.put("inwon", vo.getInwon());
+		obj.put("tutor_info_nickname", vo.getTutor_info_nickname());
+		obj.put("tutor_info_img", vo.getTutor_info_img());
+		obj.put("tutor_info_grade_total", vo.getTutor_info_grade_total());
+		obj.put("jjim_count", vo.getJjim_count());
+		
+		return obj.toJSONString();
+	}
 }
